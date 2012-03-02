494dde0b6bddd5fe4cf6afb47d8da4407de2a1d5
Fabricio Leotti <fabricio.leotti@abril.com.br>
2012-03-01 22:51:16 -0300

    Adicionado tempo inicial, final e help

--- timespent.sh
+++ timespent.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-while getopts i:t: opt
+declare -A conditions=()
+
+while getopts i:k:t:T:f:F:h opt
 do
 	case ${opt} in
-		i) IGNORE="name not like '%${OPTARG}%'";;
-		t) TASK="name like '%${OPTARG}%'";;
+		i) conditions["IGNORE"]="name not like '%${OPTARG}%'";;
+		k) conditions["TASK"]="name like '%${OPTARG}%'";;
+		f) conditions["FROM"]="start_time > STR_TO_DATE('${OPTARG}','%d/%m/%Y')";;
+		F) conditions["FROM"]="start_time > STR_TO_DATE('${OPTARG}','%d/%m/%Y %H:%i')";;
+		t) conditions["TO"]="end_time < STR_TO_DATE('${OPTARG}','%d/%m/%Y')";;
+		T) conditions["TO"]="end_time < STR_TO_DATE('${OPTARG}','%d/%m/%Yi %H:%i')";;
+		h) echo -e "USAGE: timespent\n\t\t-i ignore task\n\t\t-k task\n\t\t-f start time <dd/mm/yyyy>\n\t\t-F start time <dd/mm/yyyy hh:ii>\n\t\t-t end time <dd/mm/yyyy>\n\t\t-T end time <dd/mm/yyyy hh:ii>\n\n\t\tIf no arguments are provided, all time recorded will be printed";
+		exit 2;;
 	esac
 done
 CONDITIONAL="where"
-
-if [ "$IGNORE" != "" ]; then
-	CONDITIONAL="$CONDITIONAL $IGNORE"
-fi
-
-if [ "$TASK" != "" ]; then
-	if [ "$CONDITIONAL" != "where" ]; then
-		CONDITIONAL="$CONDITIONAL and $TASK";
-	else
-		CONDITIONAL="$CONDITIONAL $TASK";
-	fi
-fi
+itens=${#conditions[@]}
+separator="and"
+for item in "${!conditions[@]}"; do
+    let itens--
+    if [[ $itens == 0 ]]; then
+	separator=""
+    fi
+    CONDITIONAL="$CONDITIONAL ${conditions[$item]} $separator"
+done
 
 if [ "$CONDITIONAL" = "where" ]; then
 	CONDITIONAL="";
 fi
-
-#echo -e $CONDITIONAL
 
 mysql --user=root --password= timecontrol_development <<EOF
 
