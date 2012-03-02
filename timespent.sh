804dbe19c3d729ccd30fb4abe43923cbeca6125f
Fabricio Leotti <fabricio.leotti@abril.com.br>
2012-03-02 16:26:34 -0300

    Formatação da resposta

--- timespent.sh
+++ timespent.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-
+ 
 declare -A conditions=()
+declare -A queries=()
 
-while getopts i:k:t:T:f:F:h opt
+queries['REPORT']="select name, sec_to_time(sum(time_to_sec(timediff(end_time, start_time)))) as total "
+queries['GROUP']=" group by name "
+queries['ORDER']=" order by name "
+
+
+#select name, sec_to_time(sum(time_to_sec(timediff(end_time, start_time)))) as total"
+
+query_type='REPORT'
+
+while getopts i:k:t:T:f:F:h: opt
 do
 	case ${opt} in
 		i) conditions["IGNORE"]="name not like '%${OPTARG}%'";;
@@ -11,10 +21,23 @@ do
 		F) conditions["FROM"]="start_time > STR_TO_DATE('${OPTARG}','%d/%m/%Y %H:%i')";;
 		t) conditions["TO"]="end_time < STR_TO_DATE('${OPTARG}','%d/%m/%Y')";;
 		T) conditions["TO"]="end_time < STR_TO_DATE('${OPTARG}','%d/%m/%Yi %H:%i')";;
-		h) echo -e "USAGE: timespent\n\t\t-i ignore task\n\t\t-k task\n\t\t-f start time <dd/mm/yyyy>\n\t\t-F start time <dd/mm/yyyy hh:ii>\n\t\t-t end time <dd/mm/yyyy>\n\t\t-T end time <dd/mm/yyyy hh:ii>\n\n\t\tIf no arguments are provided, all time recorded will be printed";
+		h) cat <<EOF
+			USAGE: $0
+				-i ignore task
+				-k task
+				-f start time <dd/mm/yyyy>
+				-F start time <dd/mm/yyyy hh:ii>
+				-t end time <dd/mm/yyyy>
+				-T end time <dd/mm/yyyy hh:ii>
+				-r report mode
+				-l labels showed in the report (report mode only)
+				If no arguments are provided, all time recorded will be printed
+EOF
 		exit 2;;
 	esac
 done
+
+query=${queries[$query_type]}
 CONDITIONAL="where"
 itens=${#conditions[@]}
 separator="and"
@@ -30,9 +53,17 @@ if [ "$CONDITIONAL" = "where" ]; then
 	CONDITIONAL="";
 fi
 
-mysql --user=root --password= timecontrol_development <<EOF
+case ${query_type} in
+	'REPORT') query="$query from tasks $CONDITIONAL ${queries['GROUP']} ${queries['ORDER']}";;
+esac
 
-select sec_to_time(sum(time_to_sec(timediff(end_time, start_time)))) as total from tasks $CONDITIONAL;
+echo -e "###########################################################"
+echo -e "$query from tasks $CONDITIONAL"
+echo -e "###########################################################"
+
+mysql --table --user=root --password= timecontrol_development <<EOF
+
+$query;
 
 EOF
 exit 2
